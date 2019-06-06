f1fbe7fab23ed98e4d27a002d32cd45083c33486
Sergey <sergey.pro100.work@gmail.com>
2019-06-06 10:24:23 +0300

    Created an EditMeetup Component

--- src/App.svelte
+++ src/App.svelte
@@ -3,13 +3,7 @@
   import TextInput from "./UI/TextInput.svelte";
   import Button from "./UI/Button.svelte";
   import MeetupGrid from "./Meetups/MeetupGrid.svelte";
-
-  let title = "";
-  let subtitle = "";
-  let address = "";
-  let email = "";
-  let description = "";
-  let imageUrl = "";
+  import EditMeetup from "./Meetups/EditMeetup.svelte";
 
   let meetups = [
     {
@@ -36,6 +30,8 @@
       isFavorite: false
     }
   ];
+
+  let editMode;
 
   const addMeetup = () => {
     const newMeetup = {
@@ -67,51 +63,15 @@
   main {
     margin-top: 5rem;
   }
-
-  form {
-    width: 30rem;
-    max-width: 90%;
-    margin: auto;
-  }
 </style>
 
 <Header />
 
 <main>
-  <form on:submit|preventDefault={addMeetup}>
-    <TextInput
-      id="title"
-      label="Title"
-      value={title}
-      on:input={event => (title = event.target.value)} />
-    <TextInput
-      id="subtitle"
-      label="Subtitle"
-      value={subtitle}
-      on:input={event => (subtitle = event.target.value)} />
-    <TextInput
-      id="address"
-      label="Address"
-      value={address}
-      on:input={event => (address = event.target.value)} />
-    <TextInput
-      id="imageUrl"
-      label="Image URL"
-      value={imageUrl}
-      on:input={event => (imageUrl = event.target.value)} />
-    <TextInput
-      id="email"
-      label="E-Mail"
-      type="email"
-      value={email}
-      on:input={event => (email = event.target.value)} />
-    <TextInput
-      id="description"
-      label="Description"
-      controlType="textarea"
-      value={description}
-      on:input={event => (description = event.target.value)} />
-    <Button type="submit" caption="Save" />
-  </form>
-  <MeetupGrid {meetups} on:togglefavorite={toggleFavorite} />
+  <Button caption="New Meetup" on:click={() => (editMode = 'add')} />
+  {#if editMode === 'add'}
+    <EditMeetup />
+  {:else}
+    <MeetupGrid {meetups} on:togglefavorite={toggleFavorite} />
+  {/if}
 </main>
