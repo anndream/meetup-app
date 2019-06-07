6887bd41787350ff5bf803cb38aa8ef2dc47e59d
Sergey <sergey.pro100.work@gmail.com>
2019-06-07 15:24:55 +0300

    Fetching & Transforming Data

--- src/App.svelte
+++ src/App.svelte
@@ -13,6 +13,27 @@
   let page = "overview";
   let pageData = {};
 
+  fetch("https://meetups-a2909.firebaseio.com/meetups.json")
+    .then(res => {
+      if (!res.ok) {
+        throw new Error("Fetching meetups failed, please try again later!");
+      }
+      return res.json();
+    })
+    .then(data => {
+      const loadedMeetups = [];
+      for (const key in data) {
+        loadedMeetups.push({
+          ...data[key],
+          id: key
+        });
+      }
+      meetups.setMeetups(loadedMeetups);
+    })
+    .catch(err => {
+      color.log(err);
+    });
+
   const savedMeetup = event => {
     editMode = null;
     editeId = null;
