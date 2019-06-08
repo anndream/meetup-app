283fcf0a4a7f472f8f70158fdea8ad6458e9365d
Sergey <sergey.pro100.work@gmail.com>
2019-06-08 19:00:49 +0300

    Editing & Deleting Meetups

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -15,7 +15,7 @@
             id: key
           });
         }
-        return { fetchedMeetups: loadedMeetups };
+        return { fetchedMeetups: loadedMeetups.reverse() };
         // isLoading = false;
         // meetups.setMeetups(loadedMeetups.reverse());
       })
@@ -142,8 +142,7 @@
           email={meetup.contactEmail}
           address={meetup.address}
           isFav={meetup.isFavorite}
-          on:showdetails
-          on:edit />
+          on:edit={startEdit} />
       </div>
     {/each}
   </section>
